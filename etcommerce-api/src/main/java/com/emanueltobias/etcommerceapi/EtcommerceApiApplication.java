06c4b16f025331d8dd54fa182855386215ee9ad5
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-12 18:44:01 -0300

    Primeiro commit

---
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.emanueltobias.etcommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtcommerceApiApplication.class, args);
+	}
+
+}
---
+++ etcommerce-api/src/test/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanueltobias.etcommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
