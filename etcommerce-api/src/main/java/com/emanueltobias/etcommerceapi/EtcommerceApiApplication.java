bd6ca3d8a2f1fcbe6613953771baf7e35e875853
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-12 20:29:48 -0300

    Instanciando objetos e inserindo no BD em tempo de execução

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
@@ -1,13 +1,32 @@
 package com.emanueltobias.etcommerceapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.emanueltobias.etcommerceapi.model.Categoria;
+import com.emanueltobias.etcommerceapi.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class EtcommerceApiApplication {
+public class EtcommerceApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EtcommerceApiApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
