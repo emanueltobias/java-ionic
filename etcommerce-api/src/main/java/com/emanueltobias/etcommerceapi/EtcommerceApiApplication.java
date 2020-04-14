14c017b46adfaa561257e8ebb37cba175ebd726c
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-14 00:52:28 -0300

    Adicionado domain Estado e Cidade

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
@@ -8,29 +8,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.emanueltobias.etcommerceapi.model.Categoria;
+import com.emanueltobias.etcommerceapi.model.Cidade;
+import com.emanueltobias.etcommerceapi.model.Estado;
 import com.emanueltobias.etcommerceapi.model.Produto;
 import com.emanueltobias.etcommerceapi.repository.CategoriaRepository;
+import com.emanueltobias.etcommerceapi.repository.CidadeRepository;
+import com.emanueltobias.etcommerceapi.repository.EstadoRepository;
 import com.emanueltobias.etcommerceapi.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class EtcommerceApiApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EtcommerceApiApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -42,12 +49,21 @@ public class EtcommerceApiApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));			categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 
