2cd0f18ddfee630e019d1f7f649b22b9a284bfef
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-14 22:49:36 -0300

    Endpoint /clientes/{id}

---
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.emanueltobias.etcommerceapi.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emanueltobias.etcommerceapi.model.Cliente;
+import com.emanueltobias.etcommerceapi.repository.ClienteRepository;
+import com.emanueltobias.etcommerceapi.resource.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
