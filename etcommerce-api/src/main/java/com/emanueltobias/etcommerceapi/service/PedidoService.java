56ca8342c960d04d0e033862c6859d7d3dbcb9df
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-19 15:35:23 -0300

    Adicionado domain ItemPedido e ItemPedidoPK, implementado Endpoint /pedidos/{id}

---
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/PedidoService.java
@@ -0,0 +1,24 @@
+package com.emanueltobias.etcommerceapi.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emanueltobias.etcommerceapi.model.Pedido;
+import com.emanueltobias.etcommerceapi.repository.PedidoRepository;
+import com.emanueltobias.etcommerceapi.resource.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
