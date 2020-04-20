46af805f3e5be26e986471348b0214c298d2c733
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-19 22:19:29 -0300

    Deletando Categoria com DELETE

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/CategoriaService.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/CategoriaService.java
@@ -3,10 +3,12 @@ package com.emanueltobias.etcommerceapi.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.emanueltobias.etcommerceapi.model.Categoria;
 import com.emanueltobias.etcommerceapi.repository.CategoriaRepository;
+import com.emanueltobias.etcommerceapi.resource.exception.DataIntegrityException;
 import com.emanueltobias.etcommerceapi.resource.exception.ObjectNotFoundException;
 
 @Service
@@ -20,15 +22,24 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
