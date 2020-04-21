8c26378ca09b8c95ee4c7c751c57395045b80927
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-21 00:26:50 -0300

    Busca de produtos por nome e categorias

---
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.emanueltobias.etcommerceapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.emanueltobias.etcommerceapi.model.Categoria;
+import com.emanueltobias.etcommerceapi.model.Produto;
+import com.emanueltobias.etcommerceapi.repository.CategoriaRepository;
+import com.emanueltobias.etcommerceapi.repository.ProdutoRepository;
+import com.emanueltobias.etcommerceapi.resource.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+}
