2a45565fbd5f879edefd6729520a89261b874822
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-20 23:52:32 -0300

    Validação customizada de email não repetido na atualização de Cliente

---
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.emanueltobias.etcommerceapi.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.emanueltobias.etcommerceapi.dto.ClienteDTO;
+import com.emanueltobias.etcommerceapi.model.Cliente;
+import com.emanueltobias.etcommerceapi.repository.ClienteRepository;
+import com.emanueltobias.etcommerceapi.resource.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
