811c0f5322bab1eae0568d23b90f4960fbec175e
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-21 00:48:12 -0300

    Inserindo Pedido

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/model/PagamentoComCartao.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/model/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.emanueltobias.etcommerceapi.model;
 import javax.persistence.Entity;
 
 import com.emanueltobias.etcommerceapi.model.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
