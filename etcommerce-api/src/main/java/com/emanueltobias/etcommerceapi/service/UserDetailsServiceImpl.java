c9ff3a4fe341c4e9675d8e280fc7b40482ce883b
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-23 20:05:38 -0300

    Implementado autenticação e geração do token JWT

---
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.emanueltobias.etcommerceapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.emanueltobias.etcommerceapi.model.Cliente;
+import com.emanueltobias.etcommerceapi.repository.ClienteRepository;
+import com.emanueltobias.etcommerceapi.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
