08e4e85d8bda5e6e66d62ebf97b115307b695999
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-04-25 00:53:36 -0300

    Arquivos salvos no S3 AWS

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
@@ -1,11 +1,18 @@
 package com.emanueltobias.etcommerceapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.emanueltobias.etcommerceapi.service.S3Service;
+
 @SpringBootApplication
 public class EtcommerceApiApplication implements CommandLineRunner {
+	
+	
+	@Autowired
+	private S3Service s3Service;
 	
 
 	public static void main(String[] args) {
@@ -14,6 +21,7 @@ public class EtcommerceApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Dev\\temp\\teste.jpg");
 	}
 
 }
