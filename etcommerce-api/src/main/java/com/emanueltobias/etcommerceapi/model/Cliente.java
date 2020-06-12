54c48186c69223395a12aa8e5f9ba1ed90473a1a
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-06-12 00:48:12 -0300

    Padrão de nomes para imagens

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/model/Cliente.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/model/Cliente.java
@@ -53,8 +53,6 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-
-	private String imageUrl;
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -153,13 +151,6 @@ public class Cliente implements Serializable {
 		this.pedidos = pedidos;
 	}
 	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
 
 	@Override
 	public int hashCode() {
