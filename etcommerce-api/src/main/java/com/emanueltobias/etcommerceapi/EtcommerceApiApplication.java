cda6784572fcce4e0980291b5f46dd7e7fccd844
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-06-11 21:55:30 -0300

    Imagem enviada via endpoint

--- etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
+++ etcommerce-api/src/main/java/com/emanueltobias/etcommerceapi/EtcommerceApiApplication.java
@@ -1,18 +1,11 @@
 package com.emanueltobias.etcommerceapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.emanueltobias.etcommerceapi.service.S3Service;
-
 @SpringBootApplication
 public class EtcommerceApiApplication implements CommandLineRunner {
-	
-	
-	@Autowired
-	private S3Service s3Service;
 	
 
 	public static void main(String[] args) {
@@ -21,7 +14,7 @@ public class EtcommerceApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Dev\\temp\\teste.jpg");
+
 	}
 
 }
