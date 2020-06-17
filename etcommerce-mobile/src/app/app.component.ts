f11d911edb8bc3105e41b08e9db473df5fea26c7
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-06-17 00:13:58 -0300

    Criada pagina de perfil

--- etcommerce-mobile/src/app/app.component.ts
+++ etcommerce-mobile/src/app/app.component.ts
@@ -18,7 +18,8 @@ export class Etcommerce {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Perfil', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' }
     ];
 
   }
