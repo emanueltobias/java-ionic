0da50f72a73168d4d14ccf1b144b9cd067d87fd5
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-07-06 00:08:48 -0300

    Carregado os dados do produto

--- etcommerce-mobile/src/pages/produto-detail/produto-detail.ts
+++ etcommerce-mobile/src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ProdutoDetailPageModule } from './produto-detail.module';
+import { ProdutoService } from './../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
+import { ProdutoDTO } from '../../models/produto.dto';
+
 
 @IonicPage()
 @Component({
@@ -9,17 +12,30 @@ import { ProdutoDetailPageModule } from './produto-detail.module';
 })
 export class ProdutoDetailPage {
 
-  item: ProdutoDetailPageModule;
+  item: ProdutoDTO;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id: "1",
-      nome: "Mouse",
-      preco: 80.59
-    }
+    let produto_id = this.navParams.get('produto_id');
+    this.produtoService.findById(produto_id)
+      .subscribe(response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+      },
+      error => {});
+  }
+
+  getImageUrlIfExists() {
+    this.produtoService.getImageFromBucket(this.item.id)
+      .subscribe(response => {
+        this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+      },
+      error => {});
   }
 }
