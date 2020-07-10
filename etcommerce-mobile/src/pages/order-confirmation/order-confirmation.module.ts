7faf12bc87cb1321fd95a1afd772a5b0c6e73673
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-07-10 00:11:50 -0300

    Pedido salvo

--- etcommerce-mobile/src/pages/order-confirmation/order-confirmation.module.ts
+++ etcommerce-mobile/src/pages/order-confirmation/order-confirmation.module.ts
@@ -4,6 +4,9 @@ import { OrderConfirmationPage } from './order-confirmation';
 import {LOCALE_ID} from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import {registerLocaleData} from '@angular/common';
+import { PedidoService } from './../../services/domain/pedido.service';
+
+
 
 registerLocaleData(localePt, 'pt');
 
@@ -15,6 +18,7 @@ registerLocaleData(localePt, 'pt');
     IonicPageModule.forChild(OrderConfirmationPage),
   ],
   providers: [
+    PedidoService,
     {
         provide: LOCALE_ID,
         useValue: 'pt'
